56502d80f560e90b1f9251ae9450cd7afb7f9911
bakinsa <bakinasa@gmail.com>
2026-08-13 20:37:14 +0300

    Поправить конфиг

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
-    allowedHosts: ["aisudio-sirin.markovav.ru"],
+    allowedHosts: ["aistudio-sirin.markovav.ru"],
   },
 });
